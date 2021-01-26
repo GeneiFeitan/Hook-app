0174bdeff84e40b705402b7c5efceee6296e60d5
Ruben Almaguer <ruben.almaguer@elektra.com.mx>
2021-01-25 19:05:05 -0600

    useReducer

---
+++ src/components/08-useReducer/intro-reducer.js
@@ -0,0 +1,36 @@
+
+const initialState=[{
+    id:1,
+    todo:'Comprar pan',
+    done:false
+}];
+
+const todoReducer=(state=initialState ,action)=>{
+switch (action?.type) {
+    case 'agregar':
+
+        return [...state,action.payload]
+        break
+}
+
+
+    return state;
+}
+
+let todos=todoReducer();
+
+const newTodo={
+    id:2,
+    todo:'Comprar leche',
+    done:false
+}
+
+const agregarTodoAction={
+    type:'agregar',
+    payload:newTodo
+}
+
+todos=todoReducer(todos,agregarTodoAction);
+
+
+console.log(todos)
