b50b5d70935610ec1042a8a55d45a50d08d8f1dd
Ruben Almaguer <ruben.almaguer@elektra.com.mx>
2021-02-08 19:32:34 -0600

    [UPDATE] useContext

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import {TodoApp} from "./components/08-useReducer/todoApp";
 // import {Memorize} from "./components/06-memos/Memorize";
-import {MemoHook} from "./components/06-memos/MemoHook";
-import {CallbackHook} from "./components/06-memos/CallbackHook";
+// import {MemoHook} from "./components/06-memos/MemoHook";
+// import {CallbackHook} from "./components/06-memos/CallbackHook";
+import {MainApp} from "./components/09-useContext/MainApp";
 // import {FormWithCustomHook} from "./components/02-useEffect/FormWithCustomHook";
 // import {MultipleCustomHooks} from "./components/03-examples/MultipleCustomHooks";
 // import {FocusScreen} from "./components/04-useRef/FocusScreen";
@@ -11,7 +12,7 @@ import {CallbackHook} from "./components/06-memos/CallbackHook";
 // import {Layout} from "./components/05-useLayoutEffect/Layout";
 
 ReactDOM.render(
-    <CallbackHook />,
+    <MainApp />,
   document.getElementById('root')
 );
 
